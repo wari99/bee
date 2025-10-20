d4f1722012dc21082b309489b1d1747efe9642df
Mariana <57501545+wari99@users.noreply.github.com>
2025-10-20 17:21:11 -0300

    beecrowd 1142

---
+++ 1142.py
@@ -0,0 +1,15 @@
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+n = int(input())
+
+num = 1
+i = 0
+
+for i in range(n):
+    print(f"{num} {num+1} {num+2} PUM")
+    num+=4
+    
